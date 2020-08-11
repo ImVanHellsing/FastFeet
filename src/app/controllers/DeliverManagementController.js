b39124d878d01c43056f67f73632befd031ac76f
Matheud Duarte dos Santos <matusa.matheus@gmail.com>
2020-08-11 17:21:56 -0300

    Added a beta version of pick up and end ups to the DeliveryMan orders

--- src/app/controllers/DeliverManagementController.js
+++ src/app/controllers/DeliverManagementController.js
@@ -5,6 +5,7 @@ import Order from '../models/Order'
 import Deliver from '../models/Deliver'
 
 class DeliverManagementController {
+
   async index(req, res) {
     const { id } = req.params
 
@@ -38,14 +39,6 @@ class DeliverManagementController {
 
     const { id, order_id } = req.params
     const { signature_id } = req.query
-
-    /**
-     * Check if is there any signature_id on query params
-     */
-
-    if(!signature_id) {
-      return res.status(401).json({ error: `Signature_id not provided!`})
-    }
     
     /**
      * Check if the order_id belongs to the provided DeliveryMan
@@ -59,12 +52,61 @@ class DeliverManagementController {
     })
 
     if(!(id == order.deliver.id)) {
-      return res.status(401).json({ error: `You're not allowed to deliver this order!`})   
+      return res.status(401).json({ error: `You're not allowed to deliver or pickup this order!`})   
     }
 
     /**
+     * Check if the order can be PICK UP or END UP by checking de canceled_at column
+     */
+
+    if(order.canceled_at !== null) {
+      return res.status(401).json({ warning: `You can't manage a already canceled order!`})
+    }
+
+    /**
+     * Check if is there any signature_id on query params
+     */
+
+    let pickups = 0
+
+    //If there's no signature_id we try to PICK UP an order
+    if(!signature_id) {
+
+      if(order.start_date !== null) {
+        return res.status(400).json({ warning: `You've already picked up this order!`})
+      } 
+
+      //Not working yet
+      if(pickups>= 5) {
+        return res.status(400).json({ warning: `You've already picked up 5 orders today, try again tomorrow!`})
+      }
+
+      order.start_date = new Date()
+      pickups++
+
+      console.log(pickups);
+
+      await order.save()
+
+      return res.json(order)
+    }
+
+    /**
+     * You can only END UP a order if it been already PICKED UP
+     */
+
+    if(order.start_date === null) {
+      return res.status(400).json({ warning: `You can't end up a non started order!`})
+    }
+
+    if(order.end_date !== null) {
+      return res.status(400).json({ warning: `You've already end up this order!`})
+    }
+    
+    /**
      * Saving the end_date to the order
      */
+
     order.end_date = new Date()
 
     await order.save()
